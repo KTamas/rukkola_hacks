406221b9184bc7dabe162c5b2289b20c40dab40f
KTamas <ktamas@ktamas.com>
2012-08-11 23:14:43 +0200

    code cleanup, some silly micro-optimizations

--- rukkola_hacks.js
+++ rukkola_hacks.js
@@ -1,29 +1,26 @@
 function cleanup() {
   $(".container .rukknhapp").each(function(i, el) {
-    if (($(el).text().trim() === "") || ($(el).text().indexOf("happolná") > 0)) {
+    if ($(el).text().trim().indexOf('elérhető') === -1) {
       $(el).parent().parent().parent().remove();
     }
-    $('.container').css('padding-left', '0').css('margin-right', '10px');
   });
+  $('.container').css('padding-left', '0').css('margin-right', '10px');
 }
 
 function reorganize() {
   $("#books").prepend('<div class="grid-full book" id="all_books"></div>');
-  var stuff = []
-
   $("#books").find(".book_box").each(function(i, el) {
-    stuff.push(el);
+    $("#all_books").append($(el).clone());
     $(el).remove();
-  });
-
-  stuff.forEach(function(el, i) {
-    $("#all_books").append(el);
   });
 }
 
+cleanup();
+reorganize();
+
 function add_next(url) {
-  var current_page = url === "" ? 1 : url.match(/[0-9]+/)[0];
-  var next_page = parseInt(current_page)+1;
+  var current_page = url === null ? 1 : parseInt(url.match(/[0-9]+/)[0]);
+  var next_page = current_page+1;
   $("#more_books").remove();
   if (next_page <= pages) {
     $("#all_books").append('<a href="javascript:more(' + next_page + ')" id="more_books">Tovább (' + next_page + '/' + pages + ')</a>');
@@ -32,7 +29,7 @@ function add_next(url) {
 
 function more(page) {
   // note to self: firefox doesn't support window.location.origin, but fortunatelly we don't need it
-  url = window.location.pathname + "?oldal=" + page;
+  var url = window.location.pathname + "?oldal=" + page;
   $.ajax({
     url: url,
     type: "GET",
@@ -46,12 +43,9 @@ function more(page) {
   });
 }
 
-cleanup();
-reorganize();
-
 var pages = parseInt($(".last a").attr('href').match(/[0-9]+/)[0]);
 
 if (pages > 1) {
   $("nav").hide();
-  add_next("");
+  add_next(null);
 }
