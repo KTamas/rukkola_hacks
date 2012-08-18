1b775fe2507d0b9a1754cd39e3599dbd13ec98f1
KTamas <ktamas@ktamas.com>
2012-08-18 23:53:00 +0200

    well, works perfectly on collections, fails spectacularly in other situations

--- csak_happolhatoak_bookmarklet.js
+++ csak_happolhatoak_bookmarklet.js
@@ -1,10 +1,8 @@
 /*jslint browser: true, indent: 2 */
-/*global jQuery, $ */
+/*global jQuery, $, console */
 
 (function () {
   "use strict";
-  
-  $("#books").prepend('<div id="all_books" class="grid-full book"></div>');
 
   function add_happable_books_from(source) {
     $(source).find(".book_box:contains('elérhető')").each(function (i, el) {
@@ -13,60 +11,55 @@
     $('.container').css('padding-left', '0').css('margin-right', '10px');
   }
 
-  function setup() {
+  function transform_grid() {
+    $("#books").prepend('<div id="all_books" class="grid-full book"></div>');
     add_happable_books_from("#books");
     $("div.grid-full.book").not("#all_books").remove();
   }
 
-  setup();
-
-  var page_count, current_page, next_page, is_loading;
+  var page_count, initial_page, next_page, is_loading;
 
   if ($('nav').length > 0) {
+    transform_grid();
+    initial_page = parseInt($('span.page.current').text(), 10);
+    next_page = initial_page + 1;
     page_count = parseInt($(".last a").attr('href').match(/\?oldal=([\d]+)/)[1], 10);
+    $("nav").hide();
   } else {
-    page_count = 0;
+    $(".book_box").not(':contains("elérhető")').remove();
     window.scrollTo.apply(window, [0, 0]); //window.scrollTo(0) doesn't work in firefox
+    return false; //no more pages, we're done here
   }
 
   function is_window_scrollable() {
     return document.body.scrollHeight > (document.body.clientHeight + 300);
   }
 
-  function set_next(url) {
-    current_page = url === null ? 1 : parseInt(url.match(/\?oldal=([\d]+)/)[1], 10);
-    next_page = current_page === page_count ? null : current_page + 1;
-    $("#is_loading").remove();
-    if (next_page) {
-      $("#all_books").append("<div id='is_loading' style='display: none;'><b>Töltöm (" + next_page + "/" + page_count + ") </b></div>");
-    } else {
-      $("#all_books").append("<div style='clear: both;'><b>Itt a vége, fuss el véle.</b></div>");
-    }
-  }
-
-  function load_more(page) {
+  function load_more() {
     is_loading = true;
-    var url = window.location.pathname + "?oldal=" + page, previous_document_height = $(document).height();
+    var url = window.location.pathname + "?oldal=" + next_page, previous_document_height = $(document).height();
     $.ajax({
       url: url,
       type: "GET",
-      beforeSend: function () { $("#is_loading").show(); }
+      beforeSend: function () {
+        $("#all_books").append("<div id='is_loading'><b>Töltöm (" + next_page + "/" + page_count + ") </b></div>");
+      }
     }).done(function (data) {
       add_happable_books_from(data);
-      set_next(url);
+      $("#is_loading").remove();
       is_loading = false;
-      if ((previous_document_height === $(document).height() && next_page) || (!is_window_scrollable() && next_page)) {
+      next_page = next_page < page_count ? next_page + 1 : null;
+      if (!next_page) {
+        $("#all_books").append("<div style='clear: both;'><b>Itt a vége, fuss el véle.</b></div>");
+      }
+      if ((previous_document_height === $(document).height() || !is_window_scrollable()) && next_page) {
         load_more(next_page);
       }
     });
   }
 
-  if (page_count > 1) {
-    $("nav").hide();
-    set_next(null);
-    if (!is_window_scrollable() && next_page) {
-      load_more(next_page);
-    }
+  if (!is_window_scrollable() && next_page) {
+    load_more(next_page);
   }
 
   $(window).scroll(function () {
